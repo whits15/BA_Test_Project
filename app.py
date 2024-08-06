80ac78bdf2581ecf33daa664c93f09577b18947e
whits15 <80317938+whits15@users.noreply.github.com>
2024-08-05 22:43:05 -0400

    Update app.py

--- app.py
+++ app.py
@@ -148,7 +148,7 @@ def download():
     return 'No ticker provided', 400
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8000))  # Default to 8000 if PORT not set
+    port = int(os.environ.get('PORT', 10000))  # Default to 8000 if PORT not set
     app.run(host='0.0.0.0', port=port)
 
 
